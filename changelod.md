ec59b78d4956c2f157180901a38e98f76a62009a
Daara Shaw <daara@Daaras-MacBook-Air.local>
2023-10-15 13:36:50 +0100

    replace typer with argparse, refactoring

--- changelod.md
+++ changelod.md
@@ -6,5 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [0.1.1]
+
 ### Changed
-- 
+- changed from `typer` to `argparse`
+
+### Added
+- added total pounds to add to splitwise at the end
--- ynab_commands/main.py
+++ ynab_commands/main.py
@@ -1,70 +1,81 @@
+import argparse
 import os
 from datetime import datetime, timedelta
-
-import typer
-from requests import Session
+from typing import Counter
 
 from config import configs
 from models import TransactionDetail
+from requests import Session
+
 from ynab_commands.budget_api import BudgetApi
 
-config = configs[os.getenv('ENV', 'Prod')]
+CONFIG = configs[os.getenv('ENV', 'Prod')]
 
 
-def get_date(weeks: int = 4) -> str:
+def get_date(weeks: int) -> str:
     backdate = datetime.today() - timedelta(weeks=weeks)
     return str(backdate.date())
 
+def currency(amount: int) -> float:
+    return abs(amount / 1000.0)
 
-def filter_transactions(transactions: list[TransactionDetail]) -> list[TransactionDetail]:
-    filtered_list = []
-    for transaction in transactions:
-        if transaction.flag_color == "purple" and transaction.subtransactions == []:
-            filtered_list.append(transaction)
-    return filtered_list
+class BudgetSync:
+    def __init__(self) -> None:
+        self.api = BudgetApi(token=CONFIG.bearer_id, session=Session())
+        self.parser = argparse.ArgumentParser(prog="YNAB Commands", description="Split YNAB transactions")
 
+    def run(self):
+        self.parser.parse_args()
 
-def print_transaction_info(filtered_transactions: list[TransactionDetail]) -> None:
-    account_dict = {}
-    for transaction in filtered_transactions:
-        if transaction.account_name not in account_dict:
-            account_dict[transaction.account_name] = 1
-        else:
-            account_dict[transaction.account_name] += 1
-    for account, total in account_dict.items():
-        print(f"{account}: {total}")
-
-
-def main():
-    api = BudgetApi(token=config.bearer_id, session=Session())
-
-    response = api.get_transactions(
-        budget_id=config.budget_id, since_date=get_date(weeks=4)
-    )
-    filtered_transactions = filter_transactions(response.transactions)
-    total_transactions = len(filtered_transactions)
-
-    if total_transactions == 0:
-        print("No transactions found to split. Exiting.")
-        raise typer.Exit()
-
-    print_transaction_info(filtered_transactions)
-
-    should_split: bool = typer.confirm("Continue with transaction split?")
-    if not should_split:
-        raise typer.Abort()
-
-    for transaction in filtered_transactions:
-        updated_transaction = transaction.split(
-            splitwise_id=config.splitwise_id
+        response = self.api.get_transactions(
+            budget_id=CONFIG.budget_id,
+            since_date=get_date(weeks=1)
         )
-        api.update_transaction(
-            budget_id=config.budget_id,
-            transaction_id=transaction.id,
-            updated_transaction=updated_transaction,
-        )
-    print(f"Processed {total_transactions} transactions")
+
+        filtered_transactions = self.filter_transactions(response.transactions)
+    
+        if len(filtered_transactions) == 0:
+            print("No transactions found to split. Exiting.")
+            return
+    
+        self.print_transaction_info(filtered_transactions)
+    
+        continue_split: bool = input("Continue with transaction split? [y/N]: ").lower().strip() == 'y'
+        if not continue_split:
+            print("Exiting.")
+            return
+    
+        self.split_transactions(filtered_transactions)
+    
+    def filter_transactions(self, transactions: list[TransactionDetail]) -> list[TransactionDetail]:
+        return [
+            transaction for transaction in transactions 
+            if transaction.flag_color == "purple" and transaction.subtransactions == []
+        ]
+    
+    def print_transaction_info(self, filtered_transactions: list[TransactionDetail]) -> None:
+        account_counts = Counter(transaction.account_name for transaction in filtered_transactions)
+        for account, count in account_counts.items():
+            print(f"{account}: {count} transactions")
+
+    def split_transactions(self, filtered_transactions: list[TransactionDetail]) -> None:
+        transaction_total = sum(transaction.amount for transaction in filtered_transactions)
+
+        for transaction in filtered_transactions:
+            updated_transaction = transaction.split(
+                splitwise_id=CONFIG.splitwise_id
+            )
+            self.api.update_transaction(
+                budget_id=CONFIG.budget_id,
+                transaction_id=transaction.id,
+                updated_transaction=updated_transaction,
+            )
+
+        print(f"Processed {len(filtered_transactions)} transactions")
+        print(f"Add £{currency(transaction_total):.2f} to splitwise")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app = BudgetSync()
+    app.run()
+
